f4d9fd951403ee09fff89469eae0a9eec40f0838
danielvlasceanu99 <danielvlasceanu99@gmail.com>
2020-11-25 18:51:25 +0200

    Am schimbat cum dispar barele si in login

--- ProiectTocilarii_Betivani/app/src/main/java/com/example/proiect_tocilarii_betivani/LoginActivity.java
+++ ProiectTocilarii_Betivani/app/src/main/java/com/example/proiect_tocilarii_betivani/LoginActivity.java
@@ -6,36 +6,22 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.KeyEvent;
 import android.view.View;
+import android.view.WindowManager;
 import android.view.inputmethod.EditorInfo;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
-
-
-    private View decorView;
-    // Hide both the navigation bar and the status bar.
-    // SYSTEM_UI_FLAG_FULLSCREEN is only available on Android 4.1 and higher, but as
-    // a general rule, you should design your app to hide the status bar whenever you
-    // hide the navigation bar.
-
     private EditText editText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-
-        //pentru a scoate action bar
-        decorView = getWindow().getDecorView();
-        decorView.setOnSystemUiVisibilityChangeListener(new View.OnSystemUiVisibilityChangeListener(){
-            @Override
-            public void onSystemUiVisibilityChange(int visibility) {
-                if(visibility == 0)
-                    decorView.setSystemUiVisibility(hideSystemBars());
-            }
-        });
+        getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
+        // afiseaza aplicatia pe tot ecranul
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
         editText = findViewById(R.id.editLoginNumber);
 
@@ -61,22 +47,4 @@ public class LoginActivity extends AppCompatActivity {
             return false;
         }
     };
-
-    //pentru a scoate action bar
-    @Override
-    public void onWindowFocusChanged(boolean hasFocus) {
-        super.onWindowFocusChanged(hasFocus);
-        if(hasFocus){
-            decorView.setSystemUiVisibility(hideSystemBars());
-        }
-    }
-    //pentru a scoate action bar
-    private int hideSystemBars() {
-        return View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-                | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-                | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
-                | View.SYSTEM_UI_FLAG_FULLSCREEN
-                | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY;
-    }
 }
